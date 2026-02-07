1d8b97c8bf817605180621e123b3310c6259949f
e75w <elsw185@gmail.com>
2026-02-07 23:06:29 +0800

    more

--- Masterpage.Master.designer.cs
+++ Masterpage.Master.designer.cs
@@ -24,15 +24,6 @@ namespace _240795P_EvanLim
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// LinkButton1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
-
-        /// <summary>
         /// btnLogout control.
         /// </summary>
         /// <remarks>
